5919e9b89467da629bd2303d459c1eec980c8c4e
dahan <dahan0572@gmail.com>
2025-02-26 15:34:09 -0800

    first working training model... slow train time

--- models/customDataset.py
+++ models/customDataset.py
@@ -10,7 +10,11 @@ class insect_dataset(Dataset):
     def __init__(self,csv_file,root_dir, transform = None):
         self.annotations = pd.read_csv(csv_file)
         self.root_dir = root_dir
-        self.transform = transform
+        self.transform = transform if transform else transforms.Compose([
+            transforms.Resize((128, 128)),  # resize image
+            transforms.Grayscale(num_output_channels=3),  #grayscale to RGB
+            transforms.ToTensor()
+        ])
         self.classes = {0:"Butterfly", 1:"Dragonfly", 2:"Grasshopper",3:"Ladybug",4:"Mosquito"}
 
     def __len__(self):
@@ -19,14 +23,13 @@ class insect_dataset(Dataset):
     def __getitem__(self, index):
         img_path = os.path.join(self.root_dir,self.annotations.iloc[index,0])
         image = io.imread(img_path)
-        y_label = torch.tensor(int(self.annotations.iloc[index,1]))
-        image = Image.fromarray(image)
+        image = Image.fromarray(image) #numpy to PIL
+        image = image.convert("RGB") #force image to RGB
 
         if self.transform:
             image = self.transform(image)
-        else:
-            image=transforms.ToTensor()(image)
 
         label = torch.tensor(int(self.annotations.iloc[index,1]),dtype=torch.long)
 
-        return(image,y_label)
+
+        return(image,label)
--- models/train.py
+++ models/train.py
@@ -4,9 +4,11 @@ import torch.optim as optim
 import torchvision.transforms as transforms
 import torchvision
 from numpy.ma.core import shape
+from sympy.physics.vector.printing import params
 from torch.utils.data import DataLoader
 from customDataset import insect_dataset
 import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
 
 #setup device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -17,7 +19,7 @@ in_channels= 3
 num_classes = 10
 learning_rate = 1e-3
 batch_size = 32
-num_epochs = 1
+
 
 #load data
 dataset = insect_dataset(csv_file='all_insects.csv',
@@ -25,8 +27,14 @@ dataset = insect_dataset(csv_file='all_insects.csv',
                          transform = transforms.ToTensor())
 
 train_set,test_set = torch.utils.data.random_split(dataset, [3300,514])
-train_loader = DataLoader(dataset = train_set,batch_size=batch_size,shuffle = True)
-test_loader = DataLoader(dataset = train_set,batch_size=batch_size,shuffle = True)
+train_loader = DataLoader(dataset = train_set,
+                          batch_size=batch_size,
+                          shuffle = True)
+
+test_loader = DataLoader(dataset = train_set,
+                         batch_size=batch_size,
+                         shuffle = True)
+
 
 
 # Get the first batch from train_loader
@@ -72,11 +80,82 @@ class insectModelV0(nn.Module):
     def forward(self,x):
         return self.layer_stack(x)
 
-#small test run
-torch.manual_seed(42)
-
-model_0 = insectModelV0(input_shape=16384, # one for every pixel (128*128)
+#intial model
+model_0 = insectModelV0(input_shape=49152, # one for every pixel (128*128)
     hidden_units=10, # how many units in the hidden layer
     output_shape=len(class_names) # one for every class
 )
+
+# Calculate accuracy (a classification metric)
+def accuracy_fn(y_true, y_pred):
+    """Calculates accuracy between truth labels and predictions.
+
+    Args:
+        y_true (torch.Tensor): Truth labels for predictions.
+        y_pred (torch.Tensor): Predictions to be compared to predictions.
+
+    Returns:
+        [torch.float]: Accuracy value between y_true and y_pred, e.g. 78.45
+    """
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct / len(y_pred)) * 100
+    return acc
+
+
+#loss function and optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.001, momentum=0.9)
+# optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.1)
+
+#small test run
+torch.manual_seed(42)
+epochs = 3
+
+for epoch in tqdm(range(epochs)):
+    print(f"Epoch: {epoch}\n-----")
+
+    #training
+    train_loss = 0
+
+    for train_feature_batch, (X,y) in enumerate(train_loader):
+        model_0.train()
+        #forward pass
+        y_pred = model_0(X)
+        #Calculate loss per batch
+        loss = loss_fn(y_pred,y)
+        train_loss += loss #aggregates loss per epoch
+        #optimizer zero gradiant
+        optimizer.zero_grad()
+        #backward loss
+        loss.backward()
+        #optimizer step
+        optimizer.step()
+        # how many samples have been gone through
+        if train_feature_batch % 400 == 0:
+            print(f"Looked at {train_feature_batch * len(X)}/{len(train_loader.dataset)} samples")
+
+        #avg loss per batch per epoch
+        train_loss /= len(train_loader)
+
+        #testing
+        test_loss, test_acc = 0,0
+        model_0.eval()
+
+        with torch.inference_mode():
+            for X,y in test_loader:
+                #forward pass
+                test_pred = model_0(X)
+                #calculate loss
+                test_loss += loss_fn(test_pred,y)
+                #calculate accuracy
+                test_acc += accuracy_fn(y_true=y, y_pred=test_pred.argmax(dim=1))
+            #calulate test metrics
+            test_loss /= len(test_loader)
+            test_acc /= len(test_loader)
+        #print results
+        print(f"\nTrain loss: {train_loss:.5f} | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%\n")
+
+
+
+
 
